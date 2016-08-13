2b70aa604fb86ad654ad9833565125e20c797485
Contra <contra@users.noreply.github.com>
2016-08-13 16:46:18 -0400

    done

--- js/index.js
+++ js/index.js
@@ -51,13 +51,13 @@ var copyPass = new THREE.ShaderPass(THREE.CopyShader)
 // set up shader pass params
 badTVPass.uniforms.distortion.value = 4.5
 badTVPass.uniforms.distortion2.value = 1.0
-badTVPass.uniforms.speed.value = 0.03
+badTVPass.uniforms.speed.value = 0.04
 badTVPass.uniforms.rollSpeed.value = 0.03
 
 staticPass.uniforms.amount.value = 0.1
 staticPass.uniforms.size.value = 4.0
 
-rgbPass.uniforms.amount.value = 0.04
+rgbPass.uniforms.amount.value = 0.02
 rgbPass.uniforms.angle.value = 0.5
 
 filmPass.uniforms.sCount.value = 800
@@ -79,9 +79,10 @@ copyPass.renderToScreen = true
 window.addEventListener('resize', onResize, false)
 onResize()
 animate()
+worsen()
 
 function animate() {
-	shaderTime += 0.1
+	shaderTime += 0.05
 
 	badTVPass.uniforms.time.value = shaderTime
 	filmPass.uniforms.time.value = shaderTime
@@ -92,11 +93,50 @@ function animate() {
 	}
 
 	requestAnimationFrame(animate)
-  composer.render(0.1)
+  composer.render()
 }
 
 function onResize() {
 	renderer.setSize(window.innerWidth, window.innerHeight)
 	camera.aspect = window.innerWidth / window.innerHeight
 	camera.updateProjectionMatrix()
+}
+
+// fuck it up
+var timeBetween = 5000
+var worseFrames = 0
+var improveFrames = 0
+function worsen() {
+	if (++worseFrames >= timeBetween / 16) {
+		improveFrames = 0
+		console.log('switching to improve')
+		return requestAnimationFrame(improve)
+	}
+	rgbPass.uniforms.amount.value += 0.0005
+	badTVPass.uniforms.distortion.value += 0.0005
+	badTVPass.uniforms.distortion2.value += 0.0005
+
+	if (videoTexture && video.readyState === video.HAVE_ENOUGH_DATA) {
+		videoTexture.needsUpdate = true
+	}
+	requestAnimationFrame(worsen)
+	composer.render()
+}
+
+function improve() {
+	if (++improveFrames >= timeBetween / 16) {
+		worseFrames = 0
+		console.log('switching to worsen')
+		return requestAnimationFrame(worsen)
+	}
+
+	rgbPass.uniforms.amount.value -= 0.0005
+	badTVPass.uniforms.distortion.value -= 0.0005
+	badTVPass.uniforms.distortion2.value -= 0.0005
+
+	if (videoTexture && video.readyState === video.HAVE_ENOUGH_DATA) {
+		videoTexture.needsUpdate = true
+	}
+	requestAnimationFrame(improve)
+	composer.render()
 }
